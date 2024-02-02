e61bcde02d7b7d280eeb0454cabebe9ebe79fb96
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2024-02-02 16:01:22 -0300

    explanation

--- components/poc/modal.tsx
+++ components/poc/modal.tsx
@@ -47,16 +47,13 @@ export default function Modal() {
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
                 <div className="flex items-start justify-center p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Nueva tarea</h3>
+                  <h3 className="text-3xl font-semibold">New task</h3>
                 </div>
-                {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <div className="w-full max-w-xs">
-                    <form className="bg-white rounded px-8 pt-6 pb-8 mb-4">
+                    <form className="bg-white rounded px-8 pt-6 mb-4">
                       <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2">
                           Title
@@ -83,6 +80,23 @@ export default function Modal() {
                         />
                       </div>
                     </form>
+                    <p className="text-center">
+                      When you click on the button you will use a function that
+                      will save the state in the store, use both a{" "}
+                      <a
+                        className="font-semibold text-gray-900 hover:text-indigo-600"
+                        href="/doc/set"
+                      >
+                        SET
+                      </a>{" "}
+                      and a{" "}
+                      <a
+                        className="font-semibold text-gray-900 hover:text-indigo-600"
+                        href="/doc/get"
+                      >
+                        GET
+                      </a>
+                    </p>
                   </div>
                 </div>
                 <div className="flex items-center justify-center p-6 border-t border-solid border-blueGray-200 rounded-b">
