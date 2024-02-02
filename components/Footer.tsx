98f293fe8b9283a9f86637e78e6e261d8f011485
Horacio Albornoz <horaciomatiasalbornoz@gmail.com>
2024-02-02 15:48:14 -0300

    github link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="min-w-full bg-black flex justify-center items-center h-24 mx-auto px-4 text-white">
-      <a href="https://github.com/horacio-a">
+      <a href="https://github.com/horacio-a/servi-zustand-spike">
         <img src="/GitHub.png" alt="" className="max-w-10 rounded-lg" />
       </a>
     </div>
